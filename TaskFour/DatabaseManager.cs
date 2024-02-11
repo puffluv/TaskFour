7fd00451f5efaa6b566bfb41e53fde66214877c9
alibihasenov@gmail.com <alibihasenov@gmail.com>
2024-02-11 15:56:26 +0700

    Добавьте файлы проекта.

---
+++ TaskFour/DatabaseManager.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+namespace TaskFour
+{
+    public class DatabaseManager
+    {
+        private string connectionString;
+
+        // Конструктор с использованием значения по умолчанию
+        public DatabaseManager(string connectionString = "Data Source=C:\\Users\\puffluv\\source\\repos\\TaskFour\\TaskFour\\StudentsDatabase.db;Version=3;")
+        {
+            this.connectionString = connectionString;
+        }
+        public void InsertStudent(Student student)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        // Удаление из последовательности для обеспечения корректного инкремента ID
+                        using (var deleteSequenceCommand = new SQLiteCommand("DELETE FROM sqlite_sequence WHERE name='Students'", connection))
+                        {
+                            deleteSequenceCommand.ExecuteNonQuery();
+                        }
+
+                        // Вставка новой записи
+                        using (var command = new SQLiteCommand("INSERT INTO Students (Name, PhysicsGrade, MathGrade) VALUES (@Name, @PhysicsGrade, @MathGrade)", connection))
+                        {
+                            command.Parameters.AddWithValue("@Name", student.Name);
+                            command.Parameters.AddWithValue("@PhysicsGrade", student.PhysicsGrade);
+                            command.Parameters.AddWithValue("@MathGrade", student.MathGrade);
+
+                            command.ExecuteNonQuery();
+                        }
+
+                        // Если все успешно, фиксируем транзакцию
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        // Если произошла ошибка, откатываем транзакцию
+                        transaction.Rollback();
+                        Console.WriteLine($"Ошибка при вставке данных: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        public void CreateDatabase()
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("CREATE TABLE IF NOT EXISTS Students (Id INTEGER PRIMARY KEY, Name TEXT, PhysicsGrade INTEGER, MathGrade INTEGER)", connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Students";
+
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Student student = new Student();
+
+                            // Проверка наличия поля "Id" в результирующем наборе
+                            if (!reader.IsDBNull(reader.GetOrdinal("Id")))
+                            {
+                                student.Id = Convert.ToInt32(reader["Id"]);
+                            }
+
+                            // Проверка наличия поля "Name" в результирующем наборе
+                            if (!reader.IsDBNull(reader.GetOrdinal("Name")))
+                            {
+                                student.Name = reader["Name"].ToString();
+                            }
+
+                            // Проверка наличия поля "PhysicsGrade" в результирующем наборе
+                            if (!reader.IsDBNull(reader.GetOrdinal("PhysicsGrade")))
+                            {
+                                student.PhysicsGrade = Convert.ToInt32(reader["PhysicsGrade"]);
+                            }
+
+                            // Проверка наличия поля "MathGrade" в результирующем наборе
+                            if (!reader.IsDBNull(reader.GetOrdinal("MathGrade")))
+                            {
+                                student.MathGrade = Convert.ToInt32(reader["MathGrade"]);
+                            }
+
+                            students.Add(student);
+                        }
+
+                    }
+                }
+            }
+
+            return students;
+        }
+
+        public void AddStudent(string name, int physicsGrade, int mathGrade)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("INSERT INTO Students (Name, PhysicsGrade, MathGrade) VALUES (@Name, @PhysicsGrade, @MathGrade)", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@PhysicsGrade", physicsGrade);
+                    command.Parameters.AddWithValue("@MathGrade", mathGrade);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Student> ReadStudents()
+        {
+            List<Student> students = new List<Student>();
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("SELECT * FROM Students", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = Convert.ToInt32(reader["Id"]);
+                            string name = Convert.ToString(reader["Name"]);
+                            int physicsGrade = Convert.ToInt32(reader["PhysicsGrade"]);
+                            int mathGrade = Convert.ToInt32(reader["MathGrade"]);
+
+                            students.Add(new Student { Id = id, Name = name, PhysicsGrade = physicsGrade, MathGrade = mathGrade });
+                        }
+                    }
+                }
+            }
+
+            return students;
+        }
+        public void UpdateStudent(int id, string name, int physicsGrade, int mathGrade)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("UPDATE Students SET Name = @Name, PhysicsGrade = @PhysicsGrade, MathGrade = @MathGrade WHERE Id = @Id", connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@PhysicsGrade", physicsGrade);
+                    command.Parameters.AddWithValue("@MathGrade", mathGrade);
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+        public void DeleteStudent(int id)
+        {
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SQLiteCommand("DELETE FROM Students WHERE Id = @Id", connection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ TaskFour/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskFour
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private DatabaseManager databaseManager;
+        private ObservableCollection<Student> students;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            databaseManager = new DatabaseManager();
+            students = new ObservableCollection<Student>();
+            studentsDataGrid.ItemsSource = students;
+
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            students.Clear();
+            var studentsFromDb = databaseManager.GetStudents();
+            foreach (var student in studentsFromDb)
+            {
+                students.Add(student);
+            }
+        }
+
+        private void AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            // Добавление студента в базу данных
+            var newStudent = new Student { Name = "New Student" };
+            databaseManager.InsertStudent(newStudent);
+
+            // Перезагрузка данных и обновление DataGrid
+            LoadStudents();
+        }
+
+        private void UpdateStudent_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка выбранной строки
+            if (studentsDataGrid.SelectedItem != null)
+            {
+                // Получение выбранного студента из DataGrid
+                var selectedStudent = (Student)studentsDataGrid.SelectedItem;
+
+                // Обновление имени студента
+                databaseManager.UpdateStudent(selectedStudent.Id, selectedStudent.Name, selectedStudent.PhysicsGrade, selectedStudent.MathGrade);
+
+                // Перезагрузка данных и обновление DataGrid
+                LoadStudents();
+            }
+        }
+
+        private void DeleteStudent_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверка выбранной строки
+            if (studentsDataGrid.SelectedItem != null)
+            {
+                // Получение выбранного студента из DataGrid
+                var selectedStudent = (Student)studentsDataGrid.SelectedItem;
+
+                // Удаление студента из базы данных
+                databaseManager.DeleteStudent(selectedStudent.Id);
+
+                // Перезагрузка данных и обновление DataGrid
+                LoadStudents();
+            }
+        }
+    }
+}
